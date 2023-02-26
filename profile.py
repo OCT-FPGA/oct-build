b1155c147b4d972ad0727676a789744a404a4a7a
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-02-26 05:31:50 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -33,6 +33,7 @@ params = pc.bindParameters()
 # Create a XenVM
 node = request.XenVM("build-vm")
 node.xen_ptype = "build-vm"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD"
 
 # Request a specific number of VCPUs.
 node.cores = 4
