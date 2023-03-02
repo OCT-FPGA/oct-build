ae7bcd823bce9804f05ab6ec1b8865077ca6d2a5
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-03-02 04:00:15 -0500

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -5,9 +5,9 @@
 #!/usr/bin/env bash
 
 install_libs(){
-    sudo yum install ocl-icd
-    sudo yum install ocl-icd-devel
-    sudo yum install opencl-headers
+    sudo apt install ocl-icd
+    sudo apt install ocl-icd-devel
+    sudo apt install opencl-headers
     sudo /proj/octfpga-PG0/tools/Xilinx/Vitis/2022.1/scripts/installLibs.sh
 }
 install_xrt() {
