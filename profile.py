4bf277adc04b6b478d0e99a1b95b43d117f58429
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-05-26 12:06:54 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -50,7 +50,7 @@ node = request.XenVM("build-vm")
 # node = request.RawPC(name)
 node.xen_ptype = "build-vm"
 # node.hardware_type = "fpga-alveo"
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD"
 # node.exclusive = False
 
 # Request a specific number of VCPUs.
@@ -66,3 +66,5 @@ node.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-bo
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
+
+
