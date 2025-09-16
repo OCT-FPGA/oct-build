166ea30249506053d4d1b8a332028c0aec2fc329
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-09-16 00:03:11 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-#
-# (C) Copyright 2019, Xilinx, Inc.
-#
-#!/usr/bin/env bash
 
 install_libs(){
     #sudo apt install -y ocl-icd
     #sudo apt install -y ocl-icd-devel
     apt update
     apt install -y opencl-headers
-    echo "Installing Vitis ${VITISVERSION} libraries"
-    /proj/octfpga-PG0/tools/Xilinx/Vitis/${VITISVERSION}/scripts/installLibs.sh
-    bash -c "echo 'source /proj/octfpga-PG0/tools/Xilinx/Vitis/${VITISVERSION}/settings64.sh' >> /etc/profile"
+    echo "Installing Vitis ${TOOLVERSION} libraries"
+    /proj/octfpga-PG0/tools/Xilinx/Vitis/${TOOLVERSION}/scripts/installLibs.sh
+    bash -c "echo 'source /proj/octfpga-PG0/tools/Xilinx/Vitis/${TOOLVERSION}/settings64.sh' >> /etc/profile"
 }
 
 install_dev_platform(){
@@ -21,101 +17,178 @@ install_dev_platform(){
 }
 
 install_xrt() {
-    echo "Download XRT installation package"
-    wget -cO - "https://www.xilinx.com/bin/public/openDownload?filename=$XRT_PACKAGE" > /tmp/$XRT_PACKAGE
     echo "Install XRT"
-    echo "Ubuntu XRT install"
-    echo "Installing XRT dependencies..."
-    apt update
-    echo "Installing XRT package..."
-    apt install -y /tmp/$XRT_PACKAGE
-    bash -c "echo 'source /opt/xilinx/xrt/setup.sh' >> /etc/profile"
-}
-
-install_shellpkg() {
-if [[ "$SHELL" == 1 ]]; then     
-    install_u280_shell  
-fi
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+        echo "Ubuntu XRT install"
+        echo "Installing XRT dependencies..."
+        apt update
+        echo "Installing XRT package..."
+        apt install -y $XRT_BASE_PATH/$TOOLVERSION/$OSVERSION/$XRT_PACKAGE
+    fi
+    sudo bash -c "echo 'source /opt/xilinx/xrt/setup.sh' >> /etc/profile"
+    sudo bash -c "echo 'source $VITIS_BASE_PATH/$VITISVERSION/settings64.sh' >> /etc/profile"
 }
 
 check_shellpkg() {
-    PACKAGE_INSTALL_INFO=`apt list --installed 2>/dev/null | grep "$PACKAGE_NAME" | grep "$PACKAGE_VERSION"`
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+        PACKAGE_INSTALL_INFO=`apt list --installed 2>/dev/null | grep "$PACKAGE_NAME" | grep "$PACKAGE_VERSION"`
+    else
+        echo "Unsupported OS: $OSVERSION"
+        exit 1 
+    fi
 }
 
 check_xrt() {
-    XRT_INSTALL_INFO=`apt list --installed 2>/dev/null | grep "xrt" | grep "$XRT_VERSION"`
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+        XRT_INSTALL_INFO=`apt list --installed 2>/dev/null | grep "xrt" | grep "$XRT_VERSION"`
+    else
+        echo "Unsupported OS: $OSVERSION"
+        exit 1 
+    fi
+}
+
+install_xbflash() {
+    cp -r $XBFLASH_BASE_PATH/${OSVERSION} /tmp
+    echo "Installing xbflash."
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+        apt install /tmp/${OSVERSION}/*.deb
+    else
+        echo "Unsupported OS: $OSVERSION"
+        exit 1 
+    fi 
+}
+
+check_requested_shell() {
+    SHELL_INSTALL_INFO=`/opt/xilinx/xrt/bin/xbmgmt examine | grep "$DSA"`
+}
+
+check_factory_shell() {
+    SHELL_INSTALL_INFO=`/opt/xilinx/xrt/bin/xbmgmt examine | grep "$FACTORY_SHELL"`
 }
 
 install_u280_shell() {
     check_shellpkg
     if [[ $? != 0 ]]; then
-        echo "Download Shell package"
-        wget -cO - "https://www.xilinx.com/bin/public/openDownload?filename=$SHELL_PACKAGE" > /tmp/$SHELL_PACKAGE
+        # echo "Download Shell package"
+        # wget -cO - "https://www.xilinx.com/bin/public/openDownload?filename=$SHELL_PACKAGE" > /tmp/$SHELL_PACKAGE
         if [[ $SHELL_PACKAGE == *.tar.gz ]]; then
             echo "Untar the package. "
-            tar xzvf /tmp/$SHELL_PACKAGE -C /tmp/
+            tar xzvf $SHELL_BASE_PATH/$TOOLVERSION/$OSVERSION/$SHELL_PACKAGE -C /tmp/
             rm /tmp/$SHELL_PACKAGE
         fi
         echo "Install Shell"
-        echo "Install Ubuntu shell package"
-        apt-get install -y /tmp/xilinx*
+        if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+            echo "Install Ubuntu shell package"
+            apt-get install -y /tmp/xilinx*
+        elif [[ "$OSVERSION" == "centos-8" ]]; then
+            echo "Install CentOS shell package"
+            yum install -y /tmp/xilinx*
+        fi
         rm /tmp/xilinx*
     else
         echo "The package is already installed. "
     fi
 }
 
-verify_install() {
-    errors=0
-    check_xrt
-    if [ $? == 0 ] ; then
-        echo "XRT installation verified."
-    else
-        echo "XRT installation could not be verified."
-        errors=$((errors+1))
-    fi
-    check_shellpkg
-    if [ $? == 0 ] ; then
-        echo "Shell package installation verified."
-    else
-        echo "Shell package installation could not be verified."
-        errors=$((errors+1))
-    fi
-    return $errors
+flash_card() {
+    echo "Flash Card(s). "
+    /opt/xilinx/xrt/bin/xbmgmt program --base --device $PCI_ADDR
 }
-SHELL=1
+
+detect_cards() {
+    lspci > /dev/null
+    if [ $? != 0 ] ; then
+        if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+            apt-get install -y pciutils
+        elif [[ "$OSVERSION" == "centos-7" ]] || [[ "$OSVERSION" == "centos-8" ]]; then
+            yum install -y pciutils
+        fi
+    fi
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+        PCI_ADDR=$(lspci -d 10ee: | awk '{print $1}' | head -n 1)
+        if [ -n "$PCI_ADDR" ]; then
+            U280=$((U280 + 1))
+        else
+            echo "Error: No card detected."
+            exit 1
+        fi
+    fi
+}
+
+install_config_fpga() {
+    echo "Installing config-fpga."
+    cp $CONFIG_FPGA_PATH/* /usr/local/bin
+}
+
+install_libs() {
+    echo "Installing libs."
+    sudo $VITIS_BASE_PATH/$VITISVERSION/scripts/installLibs.sh
+}
+
+disable_pcie_fatal_error() {
+    echo "Disabling PCIe fatal error reporting for node: $NODE_ID"
+    sudo /proj/octfpga-PG0/tools/pcie_disable_fatal.sh $PCI_ADDR
+}
+
+XRT_BASE_PATH="/proj/octfpga-PG0/tools/deployment/xrt"
+SHELL_BASE_PATH="/proj/octfpga-PG0/tools/deployment/shell"
+XBFLASH_BASE_PATH="/proj/octfpga-PG0/tools/xbflash"
+VITIS_BASE_PATH="/proj/octfpga-PG0/tools/Xilinx/Vitis"
+CONFIG_FPGA_PATH="/proj/octfpga-PG0/tools/post-boot/u280/ubuntu-$(lsb_release -rs)"
+
 OSVERSION=`grep '^ID=' /etc/os-release | awk -F= '{print $2}'`
 OSVERSION=`echo $OSVERSION | tr -d '"'`
 VERSION_ID=`grep '^VERSION_ID=' /etc/os-release | awk -F= '{print $2}'`
 VERSION_ID=`echo $VERSION_ID | tr -d '"'`
 OSVERSION="$OSVERSION-$VERSION_ID"
 REMOTEDESKTOP=$1
-XRTVERSION=$2
-VITISVERSION=$3
+TOOLVERSION=$2
 SCRIPT_PATH=/local/repository
-COMB="${XRTVERSION}_${OSVERSION}"
+COMB="${TOOLVERSION}_${OSVERSION}"
 XRT_PACKAGE=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $1}' | awk -F= '{print $2}'`
 SHELL_PACKAGE=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $2}' | awk -F= '{print $2}'`
+DSA=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $3}' | awk -F= '{print $2}'`
 PACKAGE_NAME=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $5}' | awk -F= '{print $2}'`
 PACKAGE_VERSION=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $6}' | awk -F= '{print $2}'`
 XRT_VERSION=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $7}' | awk -F= '{print $2}'`
-U280=1
+FACTORY_SHELL="xilinx_u280_GOLDEN_8"
+NODE_ID=$(hostname | cut -d'.' -f1)
+
+detect_cards
+check_xrt
+if [ $? == 0 ]; then
+    echo "XRT is already installed."
+else
+    echo "XRT is not installed. Attempting to install XRT..."
+    install_xrt
+
+    check_xrt
+    if [ $? == 0 ]; then
+        echo "XRT was successfully installed."
+    else
+        echo "Error: XRT installation failed."
+        exit 1
+    fi
+fi
 
 install_libs
-install_xrt
-install_shellpkg
-verify_install
-    
-if [ $? == 0 ] ; then
-    echo "XRT and shell package installation successful."
+check_shellpkg
+if [ $? == 0 ]; then
+    echo "Shell is already installed."
 else
-    echo "XRT and/or shell package installation failed."
-    exit 1
+    echo "Shell is not installed. Installing shell..."
+    install_u280_shell
+    check_shellpkg
+    if [ $? == 0 ]; then
+        echo "Shell was successfully installed."
+    else
+        echo "Error: Shell installation failed."
+        exit 1
+    fi
 fi
 
 install_dev_platform
 
-echo "$REMOTEDESKTOP"
 if [ $REMOTEDESKTOP == "True" ] ; then
     echo "Installing remote desktop software"
     apt install -y ubuntu-gnome-desktop
