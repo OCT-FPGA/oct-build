075e8d140a6f798b99a411434168fdd41feb0750
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-09-17 07:42:05 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -66,16 +66,16 @@ node.ram = 1024 * params.RAM
 # Set Storage
 # node.disk = 100
 
-iface = node.addInterface()
-fsnode = request.RemoteBlockstore("fsnode", "/mydata")
-fsnode.dataset = "urn:publicid:IDN+emulab.net:octfpga+ltdataset+Xilinx"
+#iface = node.addInterface()
+#fsnode = request.RemoteBlockstore("fsnode", "/mydata")
+#fsnode.dataset = "urn:publicid:IDN+emulab.net:octfpga+ltdataset+Xilinx"
 # Now we add the link between the node and the special node
-fslink = request.Link("fslink")
-fslink.addInterface(iface)
-fslink.addInterface(fsnode.interface)
+#fslink = request.Link("fslink")
+#fslink.addInterface(iface)
+#fslink.addInterface(fsnode.interface)
 # Special attributes for this link that we must use.
-fslink.best_effort = True
-fslink.vlan_tagging = True
+#fslink.best_effort = True
+#fslink.vlan_tagging = True
 
 node.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot.sh " + str(params.remoteDesktop) + " " + params.toolVersion + " >> /local/logs/output_log.txt"))  
 
