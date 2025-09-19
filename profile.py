0f4f44d8ebfce79622aa5473d832aaabf50ef774
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-09-19 19:16:58 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,11 +1,4 @@
-"""This profile is used to instantiate a build machine in OCT. 
-
-Instructions:
-Wait for the profile instance to start, and then log in to the VM via the
-ssh port specified below.  (Note that in this case, you will need to access
-the VM through a high port on the physical host, since we have not requested
-a public IP address for the VM itself.)
-"""
+"""Use this profile to spin up a build machine in OCT.
 
 import geni.portal as portal
 import geni.rspec.pg as pg
