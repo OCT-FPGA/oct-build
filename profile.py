b1f927a95d357f7ed46f36efaaadcb55e214a70b
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2026-04-02 14:25:19 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python
+"""This profile is used to instantiate a build VM in OCT. DO NOT USE 
 
-"""
-An example of how to specify exactly how VMs are mapped to physical nodes.
+Instructions:
+Wait for the profile instance to start, and then log in to the VM via the
+ssh port specified below.  (Note that in this case, you will need to access
+the VM through a high port on the physical host, since we have not requested
+a public IP address for the VM itself.)
 """
 
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
 import geni.rspec.emulab as emulab
 
-# Function for creating VM guests with common parameters
 def mkVM(pnode, name):
     node = request.XenVM(name)
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD";
@@ -21,9 +23,42 @@ def mkVM(pnode, name):
     node.InstantiateOn(pnode)
     return node
 
-request = portal.context.makeRequestRSpec()
+# Create a Request object to start building the RSpec.
+pc = portal.Context()
+request = pc.makeRequestRSpec()
 
-#
+RAM = [16, 32, 64, 96]
+CPU = [2, 4, 8, 12]
+toolVersion = ['2023.1', '2023.2'] 
+nodeName= ['fpga-build1', 'fpga-build2', 'build']
+
+pc.defineParameter("RAM",  "RAM size (GB)",
+                   portal.ParameterType.INTEGER, RAM[0], RAM,
+                   longDescription="RAM size")
+
+pc.defineParameter("CPU",  "No: of VCPUs",
+                   portal.ParameterType.INTEGER, CPU[0], CPU,
+                   longDescription="No: of VCPUs")
+
+pc.defineParameter("toolVersion", "Tool Version",
+                   portal.ParameterType.STRING,
+                   toolVersion[0], toolVersion,
+                   longDescription="Select the tool version.")   
+
+pc.defineParameter("nodeName", "Physical host",
+                   portal.ParameterType.STRING,
+                   nodeName[0], nodeName,
+                   longDescription="Select the physical host.")  
+
+pc.defineParameter("enableRemoteDesktop", "Remote Desktop Access",
+                   portal.ParameterType.BOOLEAN, False,
+                   advanced=False,
+                   longDescription="Enable remote desktop access by installing GNOME desktop and VNC server.")
+
+params = pc.bindParameters() 
+ 
+# Create a XenVM
+
 # Set up your physical nodes as usual.
 #
 pnode1 = request.RawPC('pnode1')
@@ -34,5 +69,9 @@ pnode1.hardware_type = "build"
 #
 vm1 = mkVM("pnode1", "vm1");
 
+#node.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot.sh " + str(params.enableRemoteDesktop) + " " + params.toolVersion + " >> /local/repository/output_log.txt"))  
 
+# Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
+
+
