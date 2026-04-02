cf75baca265531f5613be2ecdca90f7e92608e4d
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2026-04-02 14:10:02 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,71 +1,38 @@
-"""This profile is used to instantiate a build VM in OCT. DO NOT USE 
+#!/usr/bin/python
 
-Instructions:
-Wait for the profile instance to start, and then log in to the VM via the
-ssh port specified below.  (Note that in this case, you will need to access
-the VM through a high port on the physical host, since we have not requested
-a public IP address for the VM itself.)
+"""
+An example of how to specify exactly how VMs are mapped to physical nodes.
 """
 
 import geni.portal as portal
-import geni.rspec.pg as pg
+import geni.rspec.pg as rspec
+import geni.rspec.emulab as emulab
 
-# Create a Request object to start building the RSpec.
-pc = portal.Context()
-request = pc.makeRequestRSpec()
+# Function for creating VM guests with common parameters
+def mkVM(pnode, name):
+    node = request.XenVM(name)
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD";
+    node.cores = 4
+    node.ram = 4096
+    node.exclusive = True
+    #
+    # This is the crux of the biscuit; tell the mapper exactly where to place the VM.
+    #
+    node.InstantiateOn(pnode)
+    return node
 
-numRAM = [16, 32, 64, 96]
-numCPU = [2, 4, 8, 12]
-vitisVersion = [('2023.1')]
-xrtVersion = [('2023.1')] 
+request = portal.context.makeRequestRSpec()
 
-pc.defineParameter("numRAM",  "RAM size (GB)",
-                   portal.ParameterType.INTEGER, numRAM[0], numRAM,
-                   longDescription="RAM size")
+#
+# Set up your physical nodes as usual.
+#
+pnode1 = request.RawPC('pnode1')
+pnode1.hardware_type = "build"
 
-pc.defineParameter("numCPU",  "No: of VCPUs",
-                   portal.ParameterType.INTEGER, numCPU[0], numCPU,
-                   longDescription="No: of VCPUs")
+#
+# Create the VMs, the first argument is which pnode to place the new VM on.
+#
+vm1 = mkVM("pnode1", "vm1");
 
-pc.defineParameter("vitisVersion", "Vitis Version",
-                   portal.ParameterType.STRING,
-                   vitisVersion[0], vitisVersion,
-                   longDescription="Select the Vitis version.")   
 
-pc.defineParameter("xrtVersion", "XRT Version",
-                   portal.ParameterType.STRING,
-                   xrtVersion[0], xrtVersion,
-                   longDescription="Select the tool version.")   
-
-pc.defineParameter("enableRemoteDesktop", "Remote Desktop Access",
-                   portal.ParameterType.BOOLEAN, False,
-                   advanced=False,
-                   longDescription="Enable remote desktop access by installing GNOME desktop and VNC server.")
-
-params = pc.bindParameters() 
- 
-# Create a XenVM
-node = request.XenVM("build-vm")
-# name = "node" + str(0)
-# node = request.RawPC(name)
-node.xen_ptype = "build"
-# node.hardware_type = "fpga-alveo"
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD"
-node.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
-
-# node.exclusive = False
-
-# Request a specific number of VCPUs.
-node.cores = params.numCPU
-
-# Request a specific amount of memory (in MB).
-node.ram = 1024*params.numRAM
-
-# Set Storage
-node.disk = 100
-
-node.addService(pg.Execute(shell="bash", command="sudo /local/repository/post-boot.sh " + str(params.enableRemoteDesktop) + " " + params.xrtVersion + " " + params.vitisVersion + " >> /local/repository/output_log.txt"))  
-
-# Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
-
